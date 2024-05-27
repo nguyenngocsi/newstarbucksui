43280e82fc62f7b6c2f4de99179c65b01f16649e
Phạm Văn Hoàng <iloveanh18@gmail.com>
2024-05-27 13:41:41 +0700

    'update'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { NewsComponent } from './news/news.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { StoresComponent } from './stores/stores.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'menu', component: MenuComponent },
+    { path: 'news', component: NewsComponent },
+    { path: 'contact', component: ContactComponent},
+    { path: 'about', component: AboutComponent},
+    { path: 'stores', component: StoresComponent},
+    { path: 'login', component: LoginComponent},
+    { path: '**', redirectTo: '/home' }
+];
